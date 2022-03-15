451ad81f61759ed64f71ce4f6a97dbcf55990b2c
Ivan Greqov <idgreq@bk.ru>
2022-03-15 11:57:38 +0300

    Add 'complete' property to Todo object

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -2,17 +2,30 @@ import generateId from './utils/id';
 
 // TODO: not sure about passing id
 function Todo(data) {
-  const { id = generateId(), title = 'Untitled', dueDate = new Date(), priority = 'low' } = data;
+  const {
+    id = generateId(),
+    title = 'Untitled',
+    dueDate = new Date(),
+    priority = 'low',
+    complete = false,
+  } = data;
 
   this.id = id;
   this.title = title;
   this.dueDate = dueDate;
   this.priority = priority;
+  this.complete = complete;
 }
+
+// TODO: add save (to storage) method too?
 
 Todo.prototype.update = function (data) {
   // TODO: assume data doesn't have undefined values
   Object.assign(this, data);
+};
+
+Todo.prototype.toggle = function () {
+  this.complete = !this.complete;
 };
 
 export default Todo;
