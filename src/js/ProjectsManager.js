f295363bcff975e157a58bad57bdd773e09264da
Ivan Greqov <idgreq@bk.ru>
2022-03-15 10:33:38 +0300

    Add function for init actions

--- src/js/ProjectsManager.js
+++ src/js/ProjectsManager.js
@@ -11,10 +11,13 @@ ProjectsManager.prototype.addProject = function (project) {
 ProjectsManager.prototype.deleteProject = function (project) {
   this.projects = this.projects.filter(({ id }) => id !== project.id);
   this.save();
+  // TODO: delete from localStorage too
 };
 
 ProjectsManager.prototype.save = function () {
   this.storage.save(this);
 };
+
+// add currentProject property?
 
 export default ProjectsManager;
--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,45 @@
 import LocStorage from './LocStorage';
 import ProjectsManager from './ProjectsManager';
 import Project from './Project';
-import Todo from './Todo';
 import demoTodosData from './data/demoTodos.json';
 import archiveTodosData from './data/archiveTodos.json';
 
 import '../css/styles.css';
 
-(function initStorage() {
+function initStorage() {
   localStorage.clear();
   const storage = new LocStorage();
   // TODO: is it a good idea?
   Project.prototype.storage = storage;
   ProjectsManager.prototype.storage = storage;
-})();
+}
 
-const projectsManager = new ProjectsManager();
+// TODO: on add todo check is there any project (create default one then!)
+
+function init() {
+  // init storage
+  initStorage();
+  // create projectsManager
+  const projectsManager = new ProjectsManager();
+  // create default project, push it to manager
+  const defautProject = new Project({ title: 'Inbox' });
+  defautProject.save();
+  projectsManager.addProject(defautProject);
+  // ? add fn to populate storage
+  defautProject.loadData(demoTodosData);
+  // ? check localStorage for data, if so get it!
+  return {
+    projectsManager,
+  };
+}
+
+const { projectsManager } = init();
 
 // TODO: auto add todo to project on create
-
-const inboxProject = new Project({ title: 'Inbox' });
-// TODO: save project immediately?
-
-demoTodosData.forEach((todo) => {
-  inboxProject.addTodo(new Todo(todo));
-});
+// TODO: save project immediately after creation?
 
 const archiveProject = new Project({ title: 'Archive' });
-
-archiveTodosData.forEach((todo) => {
-  archiveProject.addTodo(new Todo(todo));
-});
-
-projectsManager.addProject(inboxProject);
+archiveProject.loadData(archiveTodosData);
 projectsManager.addProject(archiveProject);
 
 console.log(projectsManager);
-console.log(inboxProject);
-console.log(archiveProject);
