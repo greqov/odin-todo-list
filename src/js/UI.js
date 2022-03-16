fa550a8bfbae27d0abe735cfa4854265d6b8cdb6
Ivan Greqov <idgreq@bk.ru>
2022-03-16 13:29:56 +0300

    Add handlers for 'Add project', 'Add todo' forms

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,3 +1,6 @@
+import Project from './Project';
+import Todo from './Todo';
+
 function UI() {
   //
 }
@@ -5,6 +8,35 @@ function UI() {
 UI.prototype.init = function () {
   console.log('init fn');
 };
+
+// TODO: maybe add more precise dom node, not document?
+document.addEventListener('click', (e) => {
+  const { target } = e;
+  if (target.classList.contains('js-btn-submit-project')) {
+    e.preventDefault();
+    // TODO: use form submit? not button click?
+    const form = document.querySelector('.js-form-add-project');
+    const data = Object.fromEntries(new FormData(form).entries());
+    const project = new Project(data);
+    project.save();
+    console.log(project);
+    // TODO: clear form?
+    // form.reset();
+    // TODO: render project card
+    // TODO: add to projectsManager somehow
+  }
+});
+
+const todoForm = document.querySelector('.js-form-add-todo');
+todoForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(this).entries());
+  const todo = new Todo(data);
+  console.log(todo);
+  // TODO: render todo card
+  // TODO: add todo to project
+  // TODO: save to localStorage
+});
 
 const ui = new UI();
 
